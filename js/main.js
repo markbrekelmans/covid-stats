b89b4df99b38525f3a07bf17e26f5f4f0d717c2a
option-h <69401577+option-h@users.noreply.github.com>
2020-08-16 22:52:43 +0200

    Major overhaul of main chart data and drawing logic.

--- js/main.js
+++ js/main.js
@@ -1,59 +1,93 @@
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages': ['table', 'corechart']});
 
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(initiate);
 
-function drawChart() {
-  $.get("/data/COVID-19_aantallen_gemeente_cumulatief.csv", function(csvString) {
-    var arrayData = $.csv.toArrays(csvString, {"separator" : ";", onParseValue: $.csv.hooks.castToScalar});
+function initiate() {
+  //$.get('/data/COVID-19_aantallen_gemeente_cumulatief.csv', function(csvData) {
+  $.get('https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv', function(csvData) {
+    // store the csv data in an array
+    var arrData = $.csv.toArrays(csvData, {'separator': ';', onParseValue: $.csv.hooks.castToScalar});
 
-    var data = new google.visualization.DataTable();
+    // initiate a new DataTable
+    var dt = new google.visualization.DataTable();
 
-    data.addColumn('date', 'Timestamp');
-    data.addColumn('number', 'Cases');
-    data.addColumn('number', 'Admissions');
-    data.addColumn('number', 'Deaths');
+    // add columns for time and cases
+    dt.addColumn('date', 'Timestamp');
+    dt.addColumn('number', 'Cases');
 
-    for(var row=1; row<arrayData.length; row++) {
-      data.addRow([
-          new Date(arrayData[row][0].substring(0,10)),
-          arrayData[row][4],
-          arrayData[row][5],
-          arrayData[row][6],
+    // loop through the csv data array
+    // and add the rows to the DataTable
+    for(var r=1; r<arrData.length; r++) {
+      dt.addRow([
+        new Date(arrData[r][0].substring(0,10)),
+        arrData[r][4]
       ]);
     }
 
-    var result = google.visualization.data.group(
-      data,
-      [0],
-      [
-        {'column': 1, 'aggregation': google.visualization.data.sum, 'type': 'number'},
-        {'column': 2, 'aggregation': google.visualization.data.sum, 'type': 'number'},
-        {'column': 3, 'aggregation': google.visualization.data.sum, 'type': 'number'}
-      ]
+    // aggregate sum cases by timestamp
+    var dt = google.visualization.data.group(
+      dt, [0], [{'column': 1, 'aggregation': google.visualization.data.sum, 'type': 'number'}]
     );
 
+    // initiate a new DataView
+    var dv = new google.visualization.DataView(dt);
+
+    // add a calculated column for the
+    // delta between the previous row.
+    // still need to find a way to plot
+    // a reference log line.
+    dv.setColumns([
+      1,
+      {
+        calc: deltaPrev,
+        type: 'number',
+        label: 'Nieuwe gevallen'
+      }/*,
+      {
+        calc: refValue,
+        type: 'number',
+        label: 'Reference'
+      }*/
+    ]);
+
+    function deltaPrev(dt, rowNum){
+      return Math.floor(dt.getValue(rowNum, 1) - dt.getValue(Math.max(0,rowNum-7), 1));
+    }
+
+    function refValue(dt, rowNum){
+      return Math.log(Math.max(1,rowNum)*2);
+    }
+
+    // set the chart options
     var options = {
-      title: 'Reported cases NL',
-      width: window.innerWidth,
-      height: 500,
-      vAxis: {
+      title: 'Covid-19 trend in Nederland',
+      hAxis: {
+        title: 'Totaal aantal gevallen',
         logScale: true
       },
-      series: {
-        0: {
-          color: '#FE4A49'
-        },
-        1: {
-          color: '#FED766'
-        },
-        2: {
-          color: '#009FB7'
-        }
-      }
-    };
+      vAxis: {
+        title: 'Nieuwe gevallen',
+        logScale: true
+      },
+      legend: 'none',
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
 
+    // draw a Table Chart with the DataView
     var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-    chart.draw(result, options);
+    chart.draw(dv, options);
   });
+}
+
+var resizing = false;
+
+window.onresize = function(event) {
+  resizing = true;
+
+  if($('chart_div')) {
+    initiate();
+  }
+
+  resizing = false;
 }
