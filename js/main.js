1a2f8bc948c1fd17965df9a2f2c469d6bd40b1b7
option-h <69401577+option-h@users.noreply.github.com>
2020-08-17 15:11:56 +0200

    Added ref line, improved code

--- js/main.js
+++ js/main.js
@@ -2,81 +2,110 @@ google.charts.load('current', {'packages': ['table', 'corechart']});
 
 google.charts.setOnLoadCallback(initiate);
 
+var options = {
+  title: 'Covid-19 trend in Nederland',
+  hAxis: {
+    title: 'Totaal aantal gevallen',
+    logScale: true,
+    baseline: 1,
+    format: 'short'
+  },
+  vAxis: {
+    title: 'Nieuwe gevallen',
+    logScale: true,
+    baseline: 1,
+    format: 'short'
+  },
+  legend: 'bottom',
+  interpolateNulls: true,
+  series: {
+    0: {
+      //data
+      color: '#556F44'
+    },
+    1: {
+      //ref
+      color: '#99DDC8',
+      lineDashStyle: [2,1]
+    }
+  },
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+
 function initiate() {
-  //$.get('/data/COVID-19_aantallen_gemeente_cumulatief.csv', function(csvData) {
   $.get('https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv', function(csvData) {
-    // store the csv data in an array
     var arrData = $.csv.toArrays(csvData, {'separator': ';', onParseValue: $.csv.hooks.castToScalar});
 
-    // initiate a new DataTable
-    var dt = new google.visualization.DataTable();
+    var dtA = new google.visualization.DataTable();
+    dtA.addColumn('date', 'Date');
+    dtA.addColumn('number', 'Totaal aantal gevallen');
 
-    // add columns for time and cases
-    dt.addColumn('date', 'Timestamp');
-    dt.addColumn('number', 'Cases');
+    dtA.addRows([
+      [new Date('2020-03-05'), 82],
+      [new Date('2020-03-06'), 128],
+      [new Date('2020-03-07'), 188],
+      [new Date('2020-03-08'), 265],
+      [new Date('2020-03-09'), 321],
+      [new Date('2020-03-10'), 382],
+      [new Date('2020-03-11'), 503],
+      [new Date('2020-03-12'), 503]
+    ]);
 
-    // loop through the csv data array
-    // and add the rows to the DataTable
     for(var r=1; r<arrData.length; r++) {
-      dt.addRow([
+      dtA.addRow([
         new Date(arrData[r][0].substring(0,10)),
         arrData[r][4]
-      ]);
+      ])
     }
 
-    // aggregate sum cases by timestamp
-    var dt = google.visualization.data.group(
-      dt, [0], [{'column': 1, 'aggregation': google.visualization.data.sum, 'type': 'number'}]
+    var dtA = google.visualization.data.group(
+      dtA, [0], [{'column': 1, 'aggregation': google.visualization.data.sum, 'type': 'number'}]
     );
 
-    // initiate a new DataView
-    var dv = new google.visualization.DataView(dt);
-
-    // add a calculated column for the
-    // delta between the previous row.
-    // still need to find a way to plot
-    // a reference log line.
-    dv.setColumns([
+    var dvA = new google.visualization.DataView(dtA);
+    dvA.setColumns([
       1,
       {
         calc: deltaPrev,
         type: 'number',
         label: 'Nieuwe gevallen'
-      }/*,
-      {
-        calc: refValue,
-        type: 'number',
-        label: 'Reference'
-      }*/
+      }
     ]);
 
-    function deltaPrev(dt, rowNum){
-      return Math.floor(dt.getValue(rowNum, 1) - dt.getValue(Math.max(0,rowNum-7), 1));
+    //dvA.hideRows([1]);
+
+    function deltaPrev(dtA, rowNum){
+      return (rowNum<7) ? Math.floor(dtA.getValue(rowNum, 1) - dtA.getValue(0, 1)) : Math.floor(dtA.getValue(rowNum, 1) - dtA.getValue(Math.max(0,rowNum-7), 1));
+      //return Math.floor(dtA.getValue(rowNum, 1) - dtA.getValue(Math.max(0,rowNum-7), 1));
     }
 
-    function refValue(dt, rowNum){
-      return Math.log(Math.max(1,rowNum)*2);
-    }
+    //
+    // set-up table and view B
+    //
+    $.get('/data/logref.csv', function(csvData) {
+      var arrData = $.csv.toArrays(csvData, {'separator': ';', onParseValue: $.csv.hooks.castToScalar});
+      var dtB = new google.visualization.DataTable();
+      dtB.addColumn('number', 'Totaal aantal gevallen');
+      dtB.addColumn('number', 'Exponentiele groei');
 
-    // set the chart options
-    var options = {
-      title: 'Covid-19 trend in Nederland',
-      hAxis: {
-        title: 'Totaal aantal gevallen',
-        logScale: true
-      },
-      vAxis: {
-        title: 'Nieuwe gevallen',
-        logScale: true
-      },
-      legend: 'none',
-      width: window.innerWidth,
-      height: window.innerHeight
-    }
+      for(var r=0; r<arrData.length; r++) {
+        dtB.addRow([
+          arrData[r][0],
+          arrData[r][1]
+        ])
+      }
 
-    // draw a Table Chart with the DataView
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-    chart.draw(dv, options);
+      var dvB = new google.visualization.DataView(dtB);
+
+      var dvC = new google.visualization.data.join(dvA,dvB,'full',[[0,0]],[1],[1]);
+
+      //
+      // draw the chart
+      //
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      chart.draw(dvC, options);
+    });
   });
 }
 
